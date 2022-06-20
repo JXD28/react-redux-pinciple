4137ff5b3be04e740cba544144f24880aaea453b
JXD28 <jixianda@outlook.com>
2022-06-21 01:23:36 +0800

    格式化

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,48 @@ import React, { useState, useContext } from 'react';
 
 const appContext = React.createContext(null);
 export const App = () => {
-    const [appState, setAppState] = useState({
-        user: { name: 'frank', age: 18 },
-    });
-    const contextValue = { appState, setAppState };
-    return (
-        <appContext.Provider value={contextValue}>
-            <大儿子 />
-            <二儿子 />
-            <幺儿子 />
-        </appContext.Provider>
-    );
+  const [appState, setAppState] = useState({
+    user: { name: 'frank', age: 18 },
+  });
+  const contextValue = { appState, setAppState };
+  return (
+    <appContext.Provider value={contextValue}>
+      <大儿子 />
+      <二儿子 />
+      <幺儿子 />
+    </appContext.Provider>
+  );
 };
 const 大儿子 = () => (
-    <section>
-        大儿子
-        <User />
-    </section>
+  <section>
+    大儿子
+    <User />
+  </section>
 );
 const 二儿子 = () => (
-    <section>
-        二儿子
-        <UserModifier />
-    </section>
+  <section>
+    二儿子
+    <UserModifier />
+  </section>
 );
 const 幺儿子 = () => <section>幺儿子</section>;
 
 const User = () => {
-    const contextValue = useContext(appContext);
+  const contextValue = useContext(appContext);
 
-    return <div>User:{contextValue?.appState?.user?.name}</div>;
+  return <div>User:{contextValue?.appState?.user?.name}</div>;
 };
 
 const UserModifier = () => {
-    const { appState, setAppState } = useContext(appContext);
-    const onChange = (e) => {
-        appState.user.name = e.target.value;
-        setAppState({ ...appState });
-    };
-    return (
-        <div>
-            <input value={appState?.user?.name} onChange={onChange} />
-        </div>
-    );
+  const { appState, setAppState } = useContext(appContext);
+  const onChange = (e) => {
+    appState.user.name = e.target.value;
+    setAppState({ ...appState });
+  };
+
+  return (
+    <div>
+      <input value={appState?.user?.name} onChange={onChange} />
+    </div>
+  );
 };
